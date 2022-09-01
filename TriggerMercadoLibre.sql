12a3023aef7b3a134123af19c5e8f3a2ae05d429
silesevelyn <104150849+silesevelyn@users.noreply.github.com>
2022-09-01 11:34:46 -0300

    Update TriggerMercadoLibre.sql

--- TriggerMercadoLibre.sql
+++ TriggerMercadoLibre.sql
@@ -1,5 +1,6 @@
 --Realizar un trigger que antes de realizar una compra, se verifique se posean al menos en stock la cantidad de unidades pretendidas por la compra; en caso que no sea así no se tiene que permitir la compra y se tiene que mostrar leyenda ‘Unidades Insuficientes’.
 DELIMITER $$
+DROP TRIGGER IF EXISTS BefInsCom $$
 CREATE TRIGGER  BefInsCom BEFORE INSERT ON Compra 
 FOR EACH ROW
 BEGIN 
@@ -7,16 +8,16 @@ IF(EXISTS (SELECT *
         FROM Producto 
         WHERE idProducto = NEW.idProducto
          AND 	cantidad < NEW.unidades  )) THEN
-SIGNAL SQLSTATE ‘45000’
-SET MESSAGE_TEXT = ‘Unidades Insuficientes’ ;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Unidades Insuficientes';
 END IF;
 END$$
 
 --Realizar un trigger para que al momento de hacer una compra, al confirmarse la misma se decremente del stock del producto la cantidad de unidades compradas.
 
 DELIMITER $$
+DROP TRIGGER IF EXISTS AftInsCom $$
 CREATE TRIGGER AftInsCom  AFTER INSERT ON Compra
-
 FOR EACH  ROW 
 BEGIN 
   UPDATE Producto
