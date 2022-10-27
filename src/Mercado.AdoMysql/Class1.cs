ddaff4f000b2515b42afcadf2145fa14cb4da51d
gabobill <matias.huancaet12d1@gmail.com>
2022-10-27 11:59:43 -0300

    ñ

--- src/Mercado.AdoMysql/Class1.cs
+++ src/Mercado.AdoMysql/Class1.cs
@@ -1,5 +1,17 @@
-﻿namespace Mercado.AdoMysql;
-public class Class1
-{
+﻿using System.Collections.Generic;
+using Mercado.Core;
 
+namespace Mercado.Core.Ado
+{
+    public interface IAdo
+    {
+        void AltaCliente(Cliente cliente);
+        List<Cliente> ObtenerClientes();
+
+        void AltaCompra(Compra compra);
+        List<Compra> ObtenerCompras();
+
+        void AltaProducto(Producto producto);
+        List<Producto> ObtenerProductos();
+    }
 }
--- src/Mercado.Core/Cliente.cs
+++ src/Mercado.Core/Cliente.cs
@@ -1,11 +1,33 @@
-﻿using System.Collections.Generic;
-using Mercado.Core;
-
-namespace Mercado.Core.Ado
+﻿namespace Mercado.AdoMysql;
+public class Cliente
 {
-    public interface Cliente
+    public int idCliente { get; set; }
+    public string nombre { get; set; }
+    public string apellido { get; set; }
+    public string telefono { get; set; }
+    public string email { get; set; }
+    public string usuario { get; set; }
+    public string contrasena { get; set; }
+    public Cliente() { }
+    public Cliente(int idCliente, string nombre, string apellido, string telefono, string email, string usuario, string contrasena)
     {
-        void
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.usuario = usuario;
+        this.constrasena = contrasena;
     }
 }
 
+public class Producto
+{
+    public int idProducto { get; set; }
+    public int idCliente { get; set; }
+    public decimal precio { get; set; }
+    public int cantidad { get; set; }
+    public string nombre { get; set; }
+    public datetime publicacion { get; set; }
+    public Producto() { }
+}
